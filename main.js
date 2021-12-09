f7264a6b51acf7042944cb6680005c295fe65295
Artur Karczewski <83916784+vajhero@users.noreply.github.com>
2021-12-10 00:47:11 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -49,60 +49,207 @@ function WinOpen_karczewski12() {
 function WinOpen_karczewski13() {
     window.open("zadanie9_kod.html");
 }
+
 function WinOpen_karczewski14() {
     window.open("zadanie10.html");
 }
+
 function WinOpen_karczewski15() {
     window.open("zadanie10_kod.html");
 }
+
 function WinOpen_karczewski16() {
     window.open("zadanie11.html");
 }
+
 function WinOpen_karczewski17() {
     window.open("zadanie11_kod.html");
 }
+
 function WinOpen_karczewski18() {
     window.open("zadanie12.html");
 }
+
 function WinOpen_karczewski19() {
     window.open("zadanie12_kod.html");
 }
+
 function WinOpen_karczewski20() {
     window.open("zadanie13.html");
 }
+
 function WinOpen_karczewski21() {
     window.open("zadanie15.html");
 }
+
 function WinOpen_karczewski22() {
     window.open("zadanie16.html");
 }
+
 function WinOpen_karczewski23() {
     window.open("zadanie17.html");
 }
+
 function WinOpen_karczewski24() {
     window.open("zadanie18.html");
 }
+
 function WinOpen_karczewski25() {
     window.open("zadanie19.html");
 }
+
 function WinOpen_karczewski26() {
     window.open("zadanie19_kod.html");
 }
+
 function WinOpen_karczewski27() {
     window.open("zadanie21.html");
 }
+
 function WinOpen_karczewski28() {
     window.open("zadanie21_kod.html");
 }
+
 function WinOpen_karczewski29() {
     window.open("zadanie23.html");
 }
+
 function WinOpen_karczewski30() {
     window.open("zadanie23_kod.html");
 }
+
 function WinOpen_karczewski31() {
     window.open("zadanie23_zew_kod.html");
 }
+
 function WinOpen_karczewski32() {
     window.open("zadanie13_kod.html");
+}
+
+function WinOpen_karczewski62() {
+    window.open("zadanie62.html");
+}
+
+function WinOpen_karczewski63() {
+    window.open("zadanie63.html");
+}
+
+function WinOpen_karczewski64() {
+    window.open("zadanie64.html");
+}
+
+function WinOpen_karczewski65() {
+    window.open("zadanie65.html");
+}
+
+function WinOpen_karczewski66() {
+    window.open("zadanie66.html");
+}
+
+function WinOpen_karczewski67() {
+    window.open("zadanie67.html");
+}
+
+function WinOpen_karczewski68() {
+    window.open("zadanie68.html");
+}
+
+function WinOpen_karczewski69() {
+    window.open("zadanie69.html");
+}
+
+function WinOpen_karczewski70() {
+    window.open("zadanie70.html");
+}
+
+function WinOpen_karczewski71() {
+    window.open("zadanie71.html");
+}
+
+function WinOpen_karczewski72() {
+    window.open("zadanie72.html");
+}
+
+function WinOpen_karczewski73() {
+    window.open("zadanie73.html");
+}
+
+function WinOpen_karczewski74() {
+    window.open("zadanie74.html");
+}
+
+function WinOpen_karczewski75() {
+    window.open("zadanie75.html");
+}
+
+function WinOpen_karczewski76() {
+    window.open("zadanie76.html");
+}
+
+function WinOpen_karczewski77() {
+    window.open("zadanie77.html");
+}
+
+function WinOpen_karczewski62_kod() {
+    window.open("zadanie62_kod.html");
+}
+
+function WinOpen_karczewski63_kod() {
+    window.open("zadanie63_kod.html");
+}
+
+function WinOpen_karczewski64_kod() {
+    window.open("zadanie64_kod.html");
+}
+
+function WinOpen_karczewski65_kod() {
+    window.open("zadanie65_kod.html");
+}
+
+function WinOpen_karczewski66_kod() {
+    window.open("zadanie66_kod.html");
+}
+
+function WinOpen_karczewski67_kod() {
+    window.open("zadanie67_kod.html");
+}
+
+function WinOpen_karczewski68_kod() {
+    window.open("zadanie68_kod.html");
+}
+
+function WinOpen_karczewski69_kod() {
+    window.open("zadanie69_kod.html");
+}
+
+function WinOpen_karczewski70_kod() {
+    window.open("zadanie70_kod.html");
+}
+
+function WinOpen_karczewski71_kod() {
+    window.open("zadanie71_kod.html");
+}
+
+function WinOpen_karczewski72_kod() {
+    window.open("zadanie72_kod.html");
+}
+
+function WinOpen_karczewski73_kod() {
+    window.open("zadanie73_kod.html");
+}
+
+function WinOpen_karczewski74_kod() {
+    window.open("zadanie74_kod.html");
+}
+
+function WinOpen_karczewski75_kod() {
+    window.open("zadanie75_kod.html");
+}
+
+function WinOpen_karczewski76_kod() {
+    window.open("zadanie76_kod.html");
+}
+
+function WinOpen_karczewski77_kod() {
+    window.open("zadanie77_kod.html");
 }
